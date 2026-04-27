913c5bddb3a43b495e226b29366929a1cdba7822
Alexander Boldt <boldt.alexander@outlook.com>
2026-04-27 09:54:17 +0200

    added functionality to delete a note

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,48 @@
 <template>
   <h1>Notes</h1>
 
-  <div style="width: 1000px; margin-left:auto; margin-right:auto;">
-    <div v-if="responseSuccessful">
-      <div class="note" v-for="note in notes" :key="note.id">
-        <h2>{{ note.title }}</h2>
-        <div>{{ note.description }}</div>
+  <div v-if="responseSuccessful">
+    <div class="note" v-for="note in notes" :key="note.id">
+      <div style="display: flex">
+        <div style="flex: 1">
+          <h2>{{ note.title }}</h2>
+          <div>{{ note.description }}</div>
+        </div>
+        <button class="delete" @click="deleteNote(note.id)">DELETE</button>
       </div>
     </div>
-    <div v-else>Could not load notes!</div>
   </div>
-
+  <div v-else>Could not load notes!</div>
 </template>
 
 <script setup>
 import { ref } from 'vue'
-</script>
 
-<script>
-import { ref } from 'vue'
+fetchNotes()
 
 const responseSuccessful = ref(true);
 const notes = ref();
 const responseError = ref();
 
-fetch('http://localhost:4000/api/v1/notes')
-    .then(response => {
-      responseSuccessful.value = response.status === 200;
-      return response.json();
-    }).then(data => notes.value = data)
-    .catch(error => responseError.value = error);
+const baseUrl = 'http://localhost:4000/api/v1/notes'
+
+function fetchNotes() {
+  fetch(baseUrl)
+      .then(response => {
+        responseSuccessful.value = response.status === 200;
+        return response.json();
+      }).then(data => notes.value = data)
+      .catch(error => responseError.value = error);
+}
+
+function deleteNote(id) {
+  fetch(baseUrl + '/' + id, {
+    method: 'delete'
+  }).then(response => {
+    console.log(response);
+    fetchNotes();
+  });
+}
 </script>
 
 <style>
@@ -41,6 +54,9 @@ body {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+  width: 1000px;
+  margin-left:auto;
+  margin-right:auto;
   text-align: center;
   color: darkslategrey;
   margin-top: 60px;
@@ -50,8 +66,25 @@ body {
   text-align: left;
   border: 1px solid lightgray;
   border-radius: 8px;
-  padding: 0px 12px 12px;
+  padding: 0px 0px 12px 12px;
   margin-bottom: 12px;
   color: darkslategrey;
+}
+
+.delete {
+  color: white;
+  background-color: crimson;
+  width: 68px;
+  height: 48px;
+  border-radius: 0px 8px;
+  border: none;
+}
+
+.delete:hover {
+  background-color: brown;
+}
+
+.delete:active {
+  background-color: crimson;
 }
 </style>
